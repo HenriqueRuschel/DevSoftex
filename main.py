cb46a916f2f4ffff55fe2350f677be2186ba5868
Henrique Ruschel <>
2022-08-04 13:54:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+nome=input("Por favor, digite seu nome aqui ")
+anonascimento=int(input("Por favor, digite sua data de nascimento aqui "))
+anoatual=2022
+print("Olá", nome, ",você nasceu em", anonascimento, "e sua idade é", anoatual-anonascimento)
+
+
+
